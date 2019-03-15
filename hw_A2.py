24bb461747b5788e2b663dde86b497a9fb72be6a
DashaEfr <48588806+DashaEfr@users.noreply.github.com>
2019-03-15 15:05:01 +0300

    Add files via upload

---
+++ hw_A2.py
@@ -0,0 +1,18 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 23:09:28) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> for i in range(10):
+	i=("hello world")
+	print(i)
+
+	
+hello world
+hello world
+hello world
+hello world
+hello world
+hello world
+hello world
+hello world
+hello world
+hello world
+>>> 
